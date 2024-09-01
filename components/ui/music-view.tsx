fd51534d4447cc1d12746905048def6bd4ebdb23
haruto <ynhrt1250@gmail.com>
2024-09-01 23:45:54 +0900

    delete unused

--- components/ui/music-view.tsx
+++ components/ui/music-view.tsx
@@ -22,12 +22,6 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 
 const generatePlayData = (start: Date, end: Date) => {
   const interval = eachDayOfInterval({ start, end });
